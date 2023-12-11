0b9a542d58fc5913c6914614b3ccb5854f9e5876
Bateyjosue <josuebatey19@gmail.com>
2023-12-11 15:54:24 +0200

    feat: business logic to add new category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Config, JsonDB } from 'node-json-db';
+import { Category } from './dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class CategoryService {
+  db = new JsonDB(new Config('db', true, false));
+  constructor() {}
   async getAllCategories() {
-    const db = new JsonDB(new Config('db', true, false));
-
-    const data = await db.getData('/db/category/data/');
+    const data = await this.db.getData('/db/category/data/');
     return data;
+  }
+  async addCategory(category: Category) {
+    return await this.db.push('/db/category/data[]', {
+      id: uuidv4(),
+      ...category,
+    });
   }
 }
