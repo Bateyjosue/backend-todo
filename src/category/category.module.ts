8d0c25cc74433cae5f63c3b229460e465f7c1ba5
Bateyjosue <josuebatey19@gmail.com>
2023-12-11 15:56:08 +0200

    feat: add import to the controller category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { JsonDB } from 'node-json-db';
 
 @Module({
   controllers: [CategoryController],
