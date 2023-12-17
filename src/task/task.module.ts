d84f541711c46d97e8f3d53c25bc125bf728e58b
Bateyjosue <josuebatey19@gmail.com>
2023-12-17 15:31:28 +0200

    feat: databa repository path to use relative

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
-import { DatabaseRepository } from 'src/database.repository';
+import { DatabaseRepository } from '../database.repository';
 
 @Module({
   controllers: [TaskController],
